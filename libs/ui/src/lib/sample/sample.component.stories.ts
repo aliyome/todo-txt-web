0f50fe748f3f9c66f0801a3f146331a80d9fcedf
aliyome <aliyome@gmail.com>
2020-10-23 17:27:49 +0900

    Add Storybook Integration
    yarn add --dev @nrwl/storybook
    yarn nx g @nrwl/angular:storybook-configuration ui

---
+++ libs/ui/src/lib/sample/sample.component.stories.ts
@@ -0,0 +1,15 @@
+
+import { SampleComponent } from './sample.component';
+
+export default {
+  title: 'SampleComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: SampleComponent,
+  props: {
+  }
+})
