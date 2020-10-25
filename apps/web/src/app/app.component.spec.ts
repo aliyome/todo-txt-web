a99b71160303f2b168093f4a6b3747da91f292f5
aliyome <aliyome@gmail.com>
2020-10-25 14:30:20 +0900

    fix: Broken test

--- apps/web/src/app/app.component.spec.ts
+++ apps/web/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SampleComponent } from './components/presentation/sample/sample.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, SampleComponent],
     }).compileComponents();
   });
 
