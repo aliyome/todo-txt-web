a405625967254fdbc01afe28311c434396d98493
aliyome <aliyome@gmail.com>
2020-10-25 14:58:55 +0900

    refactor: Move dir

--- apps/web/src/app/app.component.spec.ts
+++ apps/web/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { SampleComponent } from '../components/presentation/sample/sample.component';
+import { SampleComponent } from '@presentation/sample/sample.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
